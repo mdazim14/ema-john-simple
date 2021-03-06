1896a959f8f82ff1b17bdec87afe77d5d06d5400
Md Azim <mdazim.cse@gmail.com>
2021-03-07 04:39:07 +0600

    added modiule 37

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Cart = (props) => {
     const cart = props.cart;
@@ -37,6 +38,10 @@ const formatNumber = num => {
             <p><small>Shipping Cost: {shipping}</small></p>
             <p><small>Tax + Vat: {formatNumber(tax)}</small></p>
             <p>Total Price: {grandTotal}</p>
+            <br/>
+            <Link to="/review">
+                <button className="main-button">Review Order</button>
+            </Link>
         </div>
     );
 };
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -4,7 +4,7 @@ import { faCoffee, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css';
 import { Link } from 'react-router-dom';
 const Product = (props) => {
-    // console.log(props.product);
+    // console.log(props);
     const { img, name, seller, price, stock, key } = props.product;
     return (
         <div className="product">
@@ -17,10 +17,11 @@ const Product = (props) => {
                 <p><small>by: {seller}</small></p>
                 <p>${price}</p>
                 <p><small>Only {stock} left in stock - Order soon</small></p>
-                <button 
+                {props.showAddToCart  && <button  
                     className="main-button"
                     onClick={() => props.handleAddProduct(props.product)}> 
-                    <FontAwesomeIcon icon={faShoppingCart} />add to cart</button>
+                    <FontAwesomeIcon icon={faShoppingCart} />add to cart
+                    </button>}
             </div>
 
         </div>
